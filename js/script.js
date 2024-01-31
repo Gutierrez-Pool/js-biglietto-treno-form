f288f127e9dacfd71ff0e467265c10132f345c1b
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-01-31 13:27:48 +0100

    correzione

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const kmNumberElement = document.getElementById("km-number");
-const userAgeElement = document.querySelector("user-age");
+const userAgeElement = document.querySelector("#user-age");
 
 const buttonElement = document.querySelector("form input[type='button']");
 
+const resultElement = document.querySelector("#result")
 
 let basePrice;
 
@@ -10,26 +11,26 @@ let finalPrice;
 
 buttonElement.addEventListener("click",
     function () {
+        
+        basePrice = kmNumberElement.value * 0.21
 
-    basePrice = kmNumberElement.value * 0.21
-
-    finalPrice = basePrice;
+        finalPrice = basePrice;
 
 
-    if(userAgeElement.value < 18) {
+        if(userAgeElement.value < 18) {
 
-        console.log("l'utente è under 18");
-        finalPrice = basePrice - (basePrice * 0.2);
+            console.log("l'utente è under 18");
+            finalPrice = basePrice - (basePrice * 0.2);
 
-    } else if (userAgeElement.value >= 65) {
+        } else if (userAgeElement.value >= 65) {
 
-        console.log("l'utente è maggiore di 65 anni");
-        finalPrice = basePrice - (basePrice * 0.4);
+            console.log("l'utente è maggiore di 65 anni");
+            finalPrice = basePrice - (basePrice * 0.4);
+        }
+
+        finalPrice = finalPrice.toFixed(2);
+
+
+        resultElement.innerHTML += `Il prezzo del biglietto e di ${finalPrice}`;
     }
-
-    finalPrice = finalPrice.toFixed(2);
-
-
-    resultElement.innerHTML = "Il prezzo del biglietto è $ + ${finalPrice}";
-}
 )
